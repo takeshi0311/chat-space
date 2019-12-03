c93dd67165fbe0d3d62eee5ba154e4b6f10cf203
takeshi0311 <fabio.1993.2019@gmail.com>
2019-12-04 00:55:58 +0900

    chat-space-groupcreate-finish
    #what
    グループ機能の実装
    
    ＃why
    必要な機能のため

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
-  has_many :tweets
-  has_many  :users,  through:  :user_groups
-  has_many  :user_groups
+  validates :name, presence: true, uniqueness: true
+  has_many  :messages
+  has_many  :users,  through:  :group_users
+  has_many  :group_users
 end
